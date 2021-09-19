aa9e961bc3266304915e4b141dbd7ad9dd545c77
ArturoV6 <arturomadrigal@gmail.com>
2021-09-19 09:41:37 -0700

    commit de la clase de Truthy y Falsy

---
+++ Truthy_Falsy.js
@@ -0,0 +1,17 @@
+//Ejemplos para valores falsos
+
+Boolean(0); //false
+Boolean(null); //false
+Boolean(NaN); //false
+Boolean(undefined); //false
+Boolean(false); //false
+Boolean(""); //false
+
+//Ejemplos para valores verdaderos
+
+Boolean(1); //true
+Boolean(a); //true
+Boolean([]); //true aunque este vacio
+Boolean({}) //true aunque este vacio
+Boolean(function(){}) //true
+Boolean(true) //ture
