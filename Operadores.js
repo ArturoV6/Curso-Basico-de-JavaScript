f0479e9c5d8eaef8ad669ca2f32a88b41fd1cf2f
ArturoV6 <arturomadrigal@gmail.com>
2021-09-19 10:06:30 -0700

    commit para operadores

---
+++ Operadores.js
@@ -0,0 +1,35 @@
+3 + 2;
+50 - 10;
+10 * 50;
+30 / 25;
+
+"Oscar" + "Apellido";
+
+!true //negacion
+!false //true
+
+//comparadores
+
+//comparacion
+3 == "3" //true
+
+//comparacion estricta
+3 === "3" //false
+
+//signo menor
+5 < 3 //false
+
+//signo mayor
+5 > 3 //true
+
+//menor o igual
+5 <= 5 //true
+
+//mayor o igual
+5 >= 9 //false
+
+//para validar dos valores
+6 < 5 && 4 > 2
+
+//para validar si uno o otro valor es cierto
+5 < 2 || 4 > 2
