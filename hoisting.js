fa660d07345b06c54f223f21ca65b492e74621ab
ArturoV6 <arturomadrigal@gmail.com>
2021-09-19 09:11:13 -0700

    Segundo commit clase hoisting{

--- hoisting.js
+++ hoisting.js
@@ -1,2 +1,12 @@
 //hoisting es cuando las variables se delcaran antes que se procece el codigo.
 
+console.log(miNombre)
+
+var miNombre = "Oscar";
+
+//porque sale un undefined?, cuando una variable es llamada antes de ser inicializada no podra ejecutarse porque todavia no a sido detectada, 
+//asi es como se deberia escribir
+
+var miNombres = "Oscar";
+
+console.log(miNombres);
